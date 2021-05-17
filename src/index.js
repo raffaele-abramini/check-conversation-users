728f1b15baeba3fa1bc9703353c097910ea1ad75
Raffaele Abramini <rabramini@twilio.com>
2021-05-17 16:40:37 +0100

    :books: Add full logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const twilio = require("twilio");
+const Twilio = require("twilio");
 const chalk = require("chalk");
 
 const run = async () => {
-  const convo = process.argv[2];
+  const conversationSid = process.argv[2];
   let { accountSid, authToken, chatServiceSid } = {};
 
-  if (!convo) {
+  if (!conversationSid) {
     throw chalk`{redBright Please add a conversation sid to inspect}
     
     ie. {bold yarn start CHXXXXXXX}
@@ -22,6 +22,41 @@ const run = async () => {
   if (!accountSid || !authToken || !chatServiceSid) {
     throw chalk`{redBright Please make sure to populate your secret.json file with an {bold accountSid}, {bold authToken} and {bold chatServiceSid}}`;
   }
+
+  let client;
+  try {
+    client = await new Twilio(accountSid, authToken);
+  } catch (e) {
+    throw chalk`{redBright Something went wrong you getting your twilio client: ${e.message}}`;
+  }
+
+  const conversation = await client.conversations
+    .conversations(conversationSid)
+    .fetch();
+
+  console.log(
+    chalk`{bgCyanBright                         CONVERSATION                        }`
+  );
+  console.log(conversation);
+
+  const participants = await conversation.participants().list();
+  console.log(
+    chalk`{bgYellowBright                         CONVERSATION PARTICIPANTS                         }`
+  );
+  console.log(participants);
+
+  await Promise.all(
+    participants.map(async (p, i) => {
+      const user = await client.conversations.users(p.identity).fetch();
+
+      console.log(
+        chalk`{bgGreenBright                         USER ${
+          i + 1
+        }                         }`
+      );
+      console.log(user);
+    })
+  );
 };
 
 run().catch(console.log);
